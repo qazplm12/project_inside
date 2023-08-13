77dde3bbcedf6036d1bdc41bb6f7215f6759a824
lyulise <shlise77@naver.com>
2023-08-13 16:43:18 +0900

    8_13 프론트 수정중

--- src/main/project_inside_react/src/parkComps/toyProject/ToyListBoard.js
+++ src/main/project_inside_react/src/parkComps/toyProject/ToyListBoard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Button, ButtonGroup, Col, Container, Row} from "react-bootstrap";
-import TypeAheadProject from "./TypeAheadProject";
 import Thumbnail from "./Thumbnail";
 import theme from "../../theme.css";
 import TypeSearchProject from "./TypeSearchProject";
@@ -9,17 +8,16 @@ function ToyListBoard(props) {
 
     return (
         <Container>
-            <Row className={"justify-content-end"}>
-                {/* 최신 순 / 좋아요 순 / 검색창 */}
-                <Col sm>
-                    <ButtonGroup>
-                        <Button classNvame={'theme-btn'}>최신순</Button>
-                        <Button className={'theme-btn ms-3'}>좋아요 순</Button>
-                    </ButtonGroup>
+            <Row >
+                <Col className={"mt-3 d-inline d-flex justify-content-end"}></Col>
+                <Col className={"mt-3 mb-3 d-inline  d-flex justify-content-end"}>
+                    <Button>최신 순</Button>
+                    <Button className={"ms-3"}>좋아요 순</Button>
                 </Col>
-                <Col sm>
-                    <TypeSearchProject />
-                </Col>
+                <Col sm className=" pb-3"><TypeSearchProject /></Col>
+                    {/* <Button type={"btn .theme-btn me-3"}>최신순</Button> */}
+                    {/* <Button type={"btn .theme-btn ms-3"}>좋아요 순</Button> */}
+                    {/* <TypeSearchProject /> */}
             </Row>
             <Row>
                 {/*  3개 씩 리스트 뿌려 주기  이건 컴포 넌트 를 통해서 만들기*/}
